b838fd21b40fa442835e8e398bff2b48200f1221
Prinduino <70596825+Prinduino@users.noreply.github.com>
2020-10-16 17:26:06 +0200

    Update ThirdExample.ino

--- ThirdExample/ThirdExample.ino
+++ ThirdExample/ThirdExample.ino
@@ -4,20 +4,24 @@
 
 CRGB Strip1[Num_Strip1];
 
-void setup() {
+int a = 0;
+
+void setup()  {
+  Serial.begin(9600);
+
   FastLED.addLeds<NEOPIXEL,6>(Strip1, Num_Strip1);
 }
 
 void loop() {
   for(int a = 0; a < 10; a++) {
     Strip1[a] = CHSV(random(0, 255), 255, 255);
-    FastLED.show();
-    delay(5);
   }
-
+  FastLED.show();
+  delay(10);
+  
   for(int a = 10; a > 0; a--) {
     Strip1[a] = CHSV(random(0, 255), 255, 255);
-    FastLED.show();
-    delay(5);
   }
+  FastLED.show();
+  delay(10);
 }
