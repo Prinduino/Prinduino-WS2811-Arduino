d785c173e0f9a217227acfe99eaae2d8c5a964e5
Prinduino <70596825+Prinduino@users.noreply.github.com>
2020-10-16 17:25:27 +0200

    Update SecondExample.ino

--- SecondExample/SecondExample.ino
+++ SecondExample/SecondExample.ino
@@ -4,44 +4,54 @@
 
 CRGB Strip1[Num_Strip1];
 
-void setup() {
+int a = 0;
+
+void setup()  {
+  Serial.begin(9600);
+
   FastLED.addLeds<NEOPIXEL,6>(Strip1, Num_Strip1);
 }
 
 void loop() {
-  for(int a = 0; a <= 10; a++)  {
+  for(int a = 0; a <= 10; a++) {
     Strip1[a] = CHSV(255, 255, 255);
     FastLED.show();
+    Serial.println(a);
     delay(100);
   }
 
-  for(int a = 0; a <= 10; a++)  {
+  for(int a = 0; a <= 10; a++) {
     Strip1[a] = CHSV(155, 255, 255);
     FastLED.show();
+    Serial.println(a);
     delay(100);
   }
 
-  for(int a = 0; a <= 10; a++)  {
+  for(int a = 0; a <= 10; a++) {
     Strip1[a] = CHSV(100, 255, 255);
     FastLED.show();
-    delay(100);
-  }
-
-  for(int a = 10; a >= 10; a--)  {
-    Strip1[a] = CHSV(255, 255, 255);
-    FastLED.show();
+    Serial.println(a);
     delay(100);
   }
   
-  for(int a = 10; a >= 10; a--)  {
-    Strip1[a] = CHSV(155, 255, 255);
+  for(int a = 10; a >= 0; a--) {
+    Strip1[a] = CHSV(255, 255, 255);
     FastLED.show();
+    Serial.println(a);
     delay(100);
   }
-  
-  for(int a = 10; a >= 10; a--)  {
+
+  for(int a = 10; a >= 0; a--) {
+    Strip1[a] = CHSV(155, 255, 255);
+    FastLED.show();
+    Serial.println(a);
+    delay(100);
+  }
+
+  for(int a = 10; a >= 0; a--) {
     Strip1[a] = CHSV(100, 255, 255);
     FastLED.show();
+    Serial.println(a);
     delay(100);
   }
 }
